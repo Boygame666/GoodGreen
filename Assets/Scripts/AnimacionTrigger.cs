acf8efaed4afe648e5cd6892a2c73129248104d1
Boygame666 <83090077+Boygame666@users.noreply.github.com>
2023-02-20 18:50:51 +0100

    golondrina

---
+++ Assets/Scripts/AnimacionTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject BoxTrigger;
+    public GameObject Golondrina;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        Golondrina.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (BoxTrigger != null)
+            BoxTrigger.SetActive(true);
+
+        if (collision.CompareTag("Player"))
+        {
+            Golondrina.SetActive(true);
+            Destroy(BoxTrigger);
+            //Golondrina.GetComponent<Animator>().enabled = false;
+            
+        }
+
+
+    }
+
+}
