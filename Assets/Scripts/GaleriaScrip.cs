3e801a0545cf2f9ae07c15be96b6d060d08dae24
Boygame666 <83090077+Boygame666@users.noreply.github.com>
2023-02-21 12:04:11 +0100

    save

---
+++ Assets/Scripts/GaleriaScrip.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaleriaScrip : MonoBehaviour
+{
+    public int ID;
+    public string NameOfAnimal;
+    public string DescriptionOfAnimal;
+    public Sprite Imagen;
+    public int MaxNumeroFotos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
