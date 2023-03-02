2da52c7b2c2e91ee183e44d4bea2c34dd5f9d96e
Boygame666 <83090077+Boygame666@users.noreply.github.com>
2023-03-02 21:15:34 +0100

    Galeria-ui

---
+++ Assets/Scripts/UI/GaleriaSlot_UI.cs
@@ -0,0 +1,48 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaleriaSlot_UI : MonoBehaviour
+{
+    [SerializeField] private Image FotoSprite;
+    [SerializeField] private GaleriaSlots assinedSlot;
+
+    private Button button;
+
+    public GaleriaSlots AssinedSlot => assinedSlot;
+
+    private void Awake()
+    {
+        ClearSlot();
+
+        button = GetComponent<Button>();
+        button?.onClick.AddListener(OnUISlotClick);
+    }
+    public void Init(GaleriaSlots slot)
+    {
+        assinedSlot = slot;
+        UpdateSlot(slot);
+    }
+    public void UpdateSlot(GaleriaSlots slot)
+    {
+        if(slot.galeriaFoto != null) 
+        { 
+            FotoSprite.sprite = slot.galeriaFoto.Imagen;
+            FotoSprite.color = Color.white; 
+        }
+        else
+        {
+            ClearSlot();
+        }
+    }
+    public void ClearSlot()
+    {
+        assinedSlot?.clearSpace();
+        FotoSprite.sprite = null;
+        FotoSprite.color = Color.clear;
+    }
+    public void OnUISlotClick()
+    {
+
+    }
+}
