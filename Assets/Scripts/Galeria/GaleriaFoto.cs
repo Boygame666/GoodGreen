4e23a7187322dcc1007763c7e98221398d35dfaa
Boygame666 <83090077+Boygame666@users.noreply.github.com>
2023-03-02 12:02:06 +0100

    UIGaleria

--- Assets/Scripts/Galeria/GaleriaFoto.cs
+++ Assets/Scripts/Galeria/GaleriaFoto.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GaleriaFoto : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image FotoSprite;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        FotoSprite.color = Color.clear;
     }
 }
